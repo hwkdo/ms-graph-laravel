20068810efb66f02b0b234b3ce5842a22f856d59
aleex1848 <halycon01@gmail.com>
2025-11-11 12:05:44 +0100

    FormwerkMail uses formwerk_uuid

--- src/Jobs/ProcessFormwerkMail.php
+++ src/Jobs/ProcessFormwerkMail.php
@@ -3,6 +3,7 @@
 namespace Hwkdo\MsGraphLaravel\Jobs;
 
 use Hwkdo\IntranetAppFormwerk\Models\Typ;
+use Hwkdo\IntranetAppFormwerk\Models\TypHasWebhook;
 use Hwkdo\MsGraphLaravel\Interfaces\MsGraphMailServiceInterface;
 use Hwkdo\MsGraphLaravel\Models\GraphWebhookJobMapping;
 use Hwkdo\MsGraphLaravel\Models\Subscription;
@@ -12,7 +13,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Support\Facades\Validator;
 
 class ProcessFormwerkMail implements ShouldQueue
 {
@@ -41,21 +42,24 @@ class ProcessFormwerkMail implements ShouldQueue
         ];
         Log::info('ProcessFormwerkMail', $mailData);
         
-        $found = false;
-        foreach(Typ::all() as $typ)
-        {
-            if(str_contains($mailSubject,$typ->subject))
-            {
-                Log::info('ProcessFormwerkMail Betreff '.$mail->getSubject().' enthaelt '.$typ->subject);
-                $identifier = str($mailSubject)->after($typ->subject)->before('##')->value();
-                $pivot = $typ->webhooks()->latest()->wherePivot('identifier', $identifier)->first()->pivot;
+                  
+            $uuid = str($mailSubject)->between('##', '##')->value();
+            if (Validator::make(['value' => $uuid], ['value' => 'uuid'])->passes()) {
+                Log::info('ProcessFormwerkMail Betreff '.$mailSubject.' hat eine UUID');                
+                $pivot = TypHasWebhook::latest()->where('formwerk_uuid', $uuid)->first();
+                if(!$pivot) {
+                    Log::error('ProcessFormwerkMail Webhook nicht gefunden für UUID: '.$uuid);
+                    return ;
+                }
                 $pivot->update([
                     'ms_graph_mail_resource' => $mailResource
                 ]);
-                $jobClass = $typ->jobClass;
-                $jobClass::dispatch($mailResource, $identifier);
-                return;             
-            }
-        }  
+                $identifier = $pivot->identifier;
+            }                                    
+            $jobClass = $pivot->typ->jobClass;
+            $jobClass::dispatch($mailResource, $identifier, $uuid);
+            return;             
+        
+          
     }
 }
